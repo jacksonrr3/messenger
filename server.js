677ced71c047c923c9108d1cababa2e6bfb3312a
e.zaborshchikov <e.zaborshchikov@yandex.ru>
2022-10-08 22:47:25 +0300

    add handlebars

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('./dist'));
+
+// app.get('*', (req, res) => {
+//     res.sendFile(__dirname + './dist/index.html');
+// });
+
+app.listen(PORT, function () {
+    console.log(`Example app listening on port ${PORT}!`);
+});
