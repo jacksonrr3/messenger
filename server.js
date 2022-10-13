34100536f3bf9014c7de455343d998f6dbfdc7b1
e.zaborshchikov <e.zaborshchikov@yandex.ru>
2022-10-13 22:14:36 +0300

    corrections based on comments

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const PORT = 3000;
 
 app.use(express.static('./dist'));
 
-// app.get('*', (req, res) => {
-//     res.sendFile(__dirname + './dist/index.html');
-// });
-
 app.listen(PORT, function () {
     console.log(`Example app listening on port ${PORT}!`);
 });
